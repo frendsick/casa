8b502e6bad906c398e4b5b798b6913a5919a0335
Teemu Pätsi <frendsick@gmail.com>
2025-09-06 13:29:58 +0300

    feat: Parsing ops from tokens

--- casa.py
+++ casa.py
@@ -1,6 +1,7 @@
 import pathlib
 
 from casa.lexer import lex_file
+from casa.parser import parse_ops
 
 COMPILER_FOLDER = pathlib.Path(__file__).resolve().parent
 CASA_SOURCE_CODE = "test.casa"
@@ -11,6 +12,10 @@ def main():
     tokens = lex_file(code_file)
     for token in tokens:
         print(token)
+
+    ops = parse_ops(tokens)
+    for op in ops:
+        print(op)
 
 
 if __name__ == "__main__":
--- casa/common.py
+++ casa/common.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum, auto
 from pathlib import Path
-from typing import Self
+from typing import Any, Self, assert_never
 
 
 class TokenKind(Enum):
@@ -24,6 +24,10 @@ class Intrinsic(Enum):
         return cls.__members__.get(value.upper())
 
 
+class Operator(Enum):
+    PLUS = auto()
+
+
 @dataclass
 class Span:
     offset: int
@@ -41,3 +45,34 @@ class Token:
     value: str
     kind: TokenKind
     location: Location
+
+
+class OpKind(Enum):
+    ADD = auto()
+    PUSH_INT = auto()
+    PRINT = auto()
+
+
+@dataclass
+class Op:
+    value: Any
+    kind: OpKind
+    location: Location
+
+    def __post_init__(self):
+        assert len(OpKind) == 3, "Exhaustive handling for `OpKind`"
+        match self.kind:
+            # Requires `int`
+            case OpKind.PUSH_INT:
+                if not isinstance(self.value, int):
+                    raise TypeError(f"`{self.kind}` requires value of type `int`")
+            # Requires `Intrinsic`
+            case OpKind.PRINT:
+                if not isinstance(self.value, Intrinsic):
+                    raise TypeError(f"`{self.kind}` requires value of type `Intrinsic`")
+            # Requires `Operator`
+            case OpKind.ADD:
+                if not isinstance(self.value, Operator):
+                    raise TypeError(f"`{self.kind}` requires value of type `Operator`")
+            case _:
+                assert_never(self.kind)
