fe7abcfdfb44a9574c09f69566462edaf83bfbf9
Teemu Pätsi <frendsick@gmail.com>
2025-09-06 16:51:20 +0300

    refactor: Extract common Cursor

--- casa/parser.py
+++ casa/parser.py
@@ -1,69 +1,49 @@
-from dataclasses import dataclass
 from typing import assert_never
 
-from casa.common import Intrinsic, Op, Operator, OpKind, Token, TokenKind
+from casa.common import Cursor, Intrinsic, Op, Operator, OpKind, Token, TokenKind
 
-
-@dataclass
-class Parser:
-    cursor: int
-    tokens: list[Token]
-
-    def parse(self) -> list[Op]:
-        ops = []
-        while op := self.parse_op():
-            ops.append(op)
-        return ops
-
-    def is_finished(self) -> bool:
-        return self.cursor >= len(self.tokens)
-
-    def peek_op(self) -> Op | None:
-        assert len(OpKind) == 3, "Exhaustive handling for `OpKind`"
-
-        if self.is_finished():
-            return None
-
-        token = self.tokens[self.cursor]
-        match token.kind:
-            case TokenKind.EOF:
-                return None
-            case TokenKind.INTRINSIC:
-                return self.get_op_intrinsic(token)
-            case TokenKind.LITERAL:
-                return self.get_op_literal(token)
-            case TokenKind.OPERATOR:
-                return self.get_op_operator(token)
-            case _:
-                assert_never(token.kind)
-
-    def parse_op(self) -> Op | None:
-        if op := self.peek_op():
-            self.cursor += 1
-            return op
-
-    def get_op_literal(self, token: Token) -> Op:
-        if token.value.isdigit():
-            return Op(int(token.value), OpKind.PUSH_INT, token.location)
-        raise NotImplementedError(token.value)
-
-    def get_op_intrinsic(self, token: Token) -> Op:
-        assert len(Intrinsic) == 1, "Exhaustive handling for `Intrinsic`"
-        match token.value:
-            case "print":
-                return Op(Intrinsic.PRINT, OpKind.PRINT, token.location)
-            case _:
-                raise NotImplementedError(token.value)
-
-    def get_op_operator(self, token: Token) -> Op:
-        assert len(Operator) == 1, "Exhaustive handling for `Operator`"
-        match token.value:
-            case "+":
-                return Op(Operator.PLUS, OpKind.ADD, token.location)
-            case _:
-                raise NotImplementedError(token.value)
+INTRINSIC_TO_OPKIND = {
+    Intrinsic.PRINT: OpKind.PRINT,
+}
 
 
 def parse_ops(tokens: list[Token]) -> list[Op]:
-    parser = Parser(cursor=0, tokens=tokens)
-    return parser.parse()
+    cursor = Cursor(sequence=tokens)
+    ops: list[Op] = []
+    while token := cursor.pop():
+        if token.kind == TokenKind.EOF:
+            continue
+        ops.append(token_to_op(token))
+    return ops
+
+
+def token_to_op(token: Token) -> Op:
+    match token.kind:
+        case TokenKind.EOF:
+            raise ValueError(f"`{token.kind}` cannot be represented as Op")
+        case TokenKind.LITERAL:
+            return get_op_literal(token)
+        case TokenKind.INTRINSIC:
+            intrinsic = Intrinsic.from_lowercase(token.value)
+            if not intrinsic:
+                raise ValueError(f"Token `{token.value}` is not an intrinsic")
+            return Op(intrinsic, INTRINSIC_TO_OPKIND[intrinsic], token.location)
+        case TokenKind.OPERATOR:
+            return get_op_operator(token)
+        case _:
+            assert_never(token.kind)
+
+
+def get_op_literal(token: Token) -> Op:
+    if token.value.isdigit():
+        return Op(int(token.value), OpKind.PUSH_INT, token.location)
+    raise NotImplementedError(token.value)
+
+
+def get_op_operator(token: Token) -> Op:
+    assert len(Operator) == 1, "Exhaustive handling for `Operator`"
+    match token.value:
+        case "+":
+            return Op(Operator.PLUS, OpKind.ADD, token.location)
+        case _:
+            raise NotImplementedError(token.value)
