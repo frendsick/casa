b0ad0a7809b9b3fc5a868dc371c713b41c25f3b2
Teemu Pätsi <frendsick@gmail.com>
2025-09-09 13:28:19 +0300

    feat: CLI flag `--verbose`

--- casa.py
+++ casa.py
@@ -1,5 +1,7 @@
+import logging
 import pathlib
 
+from casa.cli import parse_args
 from casa.compiler import compile_bytecode
 from casa.interpreter import interpret_bytecode
 from casa.lexer import lex_file
@@ -9,26 +11,33 @@ from casa.typechecker import type_check_ops
 COMPILER_FOLDER = pathlib.Path(__file__).resolve().parent
 CASA_SOURCE_CODE = "test.casa"
 
+logger = logging.getLogger(__name__)
+
 
 def main():
     code_file = COMPILER_FOLDER / CASA_SOURCE_CODE
 
-    print(f"Lexing {CASA_SOURCE_CODE}")
+    args = parse_args()
+
+    log_level = logging.INFO if args.verbose else logging.WARNING
+    logging.basicConfig(level=log_level, format=f"[%(levelname)s] %(message)s")
+
+    logger.info(f"Lexing {CASA_SOURCE_CODE}")
     tokens = lex_file(code_file)
 
-    print("Parsing ops")
+    logger.info("Parsing ops")
     ops = parse_ops(tokens)
 
-    print("Resolving identifiers")
+    logger.info("Resolving identifiers")
     resolve_identifiers(ops)
 
-    print("Type checking ops")
+    logger.info("Type checking ops")
     type_check_ops(ops)
 
-    print("Compiling bytecode")
+    logger.info("Compiling bytecode")
     bytecode = compile_bytecode(ops, initialize_globals=True)
 
-    print("Interpreting bytecode")
+    logger.info("Interpreting bytecode")
     interpret_bytecode(bytecode)
 
 
