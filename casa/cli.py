d0074ffd71745be00ef40c37cc3dd0e0a18c61b0
Teemu Pätsi <frendsick@gmail.com>
2025-09-09 13:28:19 +0300

    feat: CLI flag `--verbose`

---
+++ casa/cli.py
@@ -0,0 +1,6 @@
+import argparse
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
+    return parser.parse_args()
